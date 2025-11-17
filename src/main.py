13945ed4ca3826835c87a4dac294f3cb1cd4a409
RomanD <x43521633@gmail.com>
2025-11-17 16:28:23 +0300

    feat: main.py is added

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from src.core.config import settings
+
+app = FastAPI(title="Baby Food CAtalog")
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok", "db_url": settings.DATABASE_URL}
