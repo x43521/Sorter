3fe11679e531209c8dc66481dca999e3d63d8763
RomanD <x43521633@gmail.com>
2025-11-21 14:22:25 +0300

    fix: pydantic settings have been fixed

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 
 class Settings(BaseSettings):
@@ -7,9 +8,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-
-class Config:
-    env_file = ".env"
+    model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
