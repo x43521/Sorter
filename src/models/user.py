57433f0ccf1b3179024a5eac9270ae5d4561a1f5
RomanD <x43521633@gmail.com>
2025-12-04 13:15:39 +0300

    fix: added a ban on empty values in database columns

--- src/models/user.py
+++ src/models/user.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Index
 from src.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    __table_args__ = (Index("ix_users_email_unique", "email", unique=True),)
