fd04d589803c6fc0f66eb35d60d8b622793ec781
RomanD <x43521633@gmail.com>
2025-12-04 15:10:06 +0300

    feat: the security file has been added

---
+++ src/core/security.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+from src.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=30))
+    to_encode.update({"exp": expire})
+    return jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+        )
