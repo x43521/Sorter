7a02015df66878bb896c337fd7721055cd8df165
RomanD <x43521633@gmail.com>
2025-12-05 15:47:37 +0300

    feat: the router is connected

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from src.core.config import settings
-
+from src.api.auth import router as auth_router
 
 app = FastAPI(title="Baby Food Catalog")
 
+app.include_router(auth_router)
 
 @app.get("/health")
 def health_check():
