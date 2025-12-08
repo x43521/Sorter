f08951485eda5aefa2a3bde2db0f601ef5aa5aec
RomanD <x43521633@gmail.com>
2025-12-08 15:56:05 +0300

    fix: the incorrect "from.import" has been removed

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,12 @@
 import sys
 from pathlib import Path
-
 sys.path.insert(0, str(Path(__file__).parents[1]))
 
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-from src.models import Base
 from src.core.config import settings
+from src.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
