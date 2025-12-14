0947fd867f9a41dfbd363e465b1e87a94b97703e
RomanD <x43521633@gmail.com>
2025-12-14 14:51:56 +0300

    fix: autoflush instead of autodlush in sessionlocal

--- src/database.py
+++ src/database.py
@@ -5,6 +5,6 @@ from src.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autodlush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
