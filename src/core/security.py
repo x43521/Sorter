4bda74e18f0ea59174edac092d4d7fd5847a1b2f
RomanD <x43521633@gmail.com>
2025-12-17 16:49:14 +0300

    fix: transfered to bcrypt

--- src/core/security.py
+++ src/core/security.py
@@ -2,16 +2,22 @@ from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
 from src.core.config import settings
+import bcrypt
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    pwd_bytes = plain_password.encode('utf-8')
+    hash_bytes = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(pwd_bytes, hash_bytes)
 
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode('utf-8')
 
 
 def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
